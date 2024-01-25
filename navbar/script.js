169f654ff86be5d016195068aaffb74316d8aa36
wnsdn00 <114475584+wnsdn00@users.noreply.github.com>
2024-01-25 23:14:51 +0900

    Add files via upload

---
+++ navbar/script.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-icon').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+});
